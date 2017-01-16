25d0e29ac81b831eadb9ed34fd16cb327f7b5e98
Sebastien Andrivet <sebastien@advtools.com>
2017-01-16 21:55:36 +0100

    Upgrade versions

--- setup.py
+++ setup.py
@@ -14,16 +14,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='wagtail-markdown',
-    version='0.4.1',
+    version='0.5.0',
     description='Markdown support for Wagtail',
-    long_description="Provides Markdown page field and streamfield block for Wagtail.  More info: https://github.com/torchbox/wagtail-markdown/blob/master/README.md",
+    long_description="Provides Markdown page field and streamfield block for Wagtail.  More info: https://github.com/andrivet/wagtail-markdown/blob/master/README.md",
     author='Felicity Tarnell',
     author_email='felicity@torchbox.com',
-    url='https://github.com/torchbox/wagtail-markdown',
+    url='https://github.com/andrivet/wagtail-markdown',
 
     install_requires=[
-        'Markdown==2.6.2',
-        'bleach==1.4.2',
+        'Markdown==2.6.7',
+        'bleach==1.5.0',
     ],
     license='zlib license',
 
